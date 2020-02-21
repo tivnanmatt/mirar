398c7512d48a8676193a77680bafbdbf35192791
Matt Tivnan <tivnanmatt@gmail.com>
2020-02-21 08:36:47 -0600

    working with iterative huber

--- __init__.py
+++ __init__.py
@@ -21,6 +21,8 @@ class mirarLinearOperator(abc.ABC):
 class transposedOperator(mirarLinearOperator):
     def __init__(self,operator):
         self.operator = operator
+    def isTransposed(self):
+        return True
     def dot(self,x):
         return self.operator.Tdot(x)
     def Tdot(self,y):
@@ -28,16 +30,17 @@ class transposedOperator(mirarLinearOperator):
 
 class linearSum(mirarLinearOperator):
     def __init__(self,operators):
-        def dot(self,x):
-            xOut = 0.0
-            for iOperator in np.arange(len(self.operators)):
-                xOut = xOut + self.operators[iOperator].dot(x)
-            return xOut
-        def dot(self,y):
-            yOut = 0.0
-            for iOperator in np.arange(len(self.operators)):
-                yOut = yOut + self.operators[iOperator].Tdot(y)
-            return yOut
+        self.operators = operators
+    def dot(self,x):
+        xOut = 0.0
+        for iOperator in np.arange(len(self.operators)):
+            xOut = xOut + self.operators[iOperator].dot(x)
+        return xOut
+    def Tdot(self,y):
+        yOut = 0.0
+        for iOperator in np.arange(len(self.operators)):
+            yOut = yOut + self.operators[iOperator].Tdot(y)
+        return yOut
 
 class linearProduct(mirarLinearOperator):
     def __init__(self,operators):
@@ -91,9 +94,33 @@ class convolutionFilter(mirarLinearOperator):
     def __init__(self,kernel):
         self.kernel = kernel
     def dot(self,x):
-        return scipy.convolve(x,kernel)
+        return scipy.signal.convolve(x,self.kernel, mode='same')
     def Tdot(self,y):
-        return scipy.convolve(y,np.flip(kernel))
+        return scipy.signal.convolve(y,np.flip(self.kernel), mode='same')
+
+class reorder(mirarLinearOperator):
+    def __init__(self, forwardReorder):
+        reverseOrder = 0*newOrder
+        for iEl in np.newOrder:
+            reverseReorder[iEl] = np.where(forwardReorder==iEl)
+        self.forwardReorder = forwardReorder
+        self.reverseReorder = reverseReorder
+    def dot(self,x):
+        return x.transpose(forwardReorder)
+    def Tdot(self,y):
+        return y.transpose(reverseReorder)
+
+class reshape(mirarLinearOperator):
+    def __init__(self,forwardReshape, reverseReshape):
+        self.forwardReshape = forwardReshape
+        self.reverseReshape = reverseReshape
+    def dot(self,x):
+        return x.reshape(self.forwardReshape)
+    def Tdot(self,y):
+        return y.reshape(self.reverseReshape)
+
+
+
 
 
 class mirarObjectiveFunction(abc.ABC):
@@ -106,6 +133,9 @@ class mirarObjectiveFunction(abc.ABC):
     @abc.abstractmethod
     def hessian(self,x): 
         pass
+    def hessp(self,x,p):
+        hess = self.hessian(x)
+        return hess.dot(p)
 
 class jointObjective(mirarObjectiveFunction):
     def __init__(self,objectiveTerms):
@@ -119,29 +149,87 @@ class jointObjective(mirarObjectiveFunction):
         grad = 0.0
         for iObj in np.arange(len(self.objectiveTerms)):
             grad = grad + self.objectiveTerms[iObj].gradient(x)
+        return grad
     def hessian(self,x):
-        hessianOperators = tuple(obj.hessian(x) for obj in np.arange(len(self.objectiveTerms)))
+        hessianOperators = tuple(obj.hessian(x) for obj in self.objectiveTerms)
         return linearSum(hessianOperators)
+
+class lexicoVersion(mirarObjectiveFunction):
+    def __init__(self, originalObjective, shapeX):
+        self.originalObjective = originalObjective
+        forwardReshape = shapeX
+        reverseReshape = tuple([np.prod(shapeX)])
+        self.U = reshape(forwardReshape, reverseReshape)
+    def objective(self,x):
+        return self.originalObjective.objective(self.U.dot(x))
+    def gradient(self,x):
+        return self.U.Tdot(self.originalObjective.gradient(self.U.dot(x)))
+    def hessian(self,x):
+        return linearProduct( (self.U.transpose(), self.originalObjective.hessian(self.U.dot(x)), self.U) )
 
 class multivariateGaussian(mirarObjectiveFunction):
     def __init__(self,meanX, invCovX):
         self.meanX = meanX 
         self.invCovX = invCovX
-    def objective(x):
-        return 0.5*(x - self.meanX)*self.covX.dot(x - self.meanX) 
-    def gradient(x):
-        return self.covX.dot(x - self.meanX)
-    def hessian(x):
-        return self.covX
+    def objective(self, x):
+        obj = 0.5*(x - self.meanX)*self.invCovX.dot(x - self.meanX) 
+        return np.sum(obj)
+    def gradient(self, x):
+        return self.invCovX.dot(x - self.meanX)
+    def hessian(self, x):
+        return self.invCovX
 
 class quadraticSmoothness(mirarObjectiveFunction):
     def __init__(self, beta, kernel):
         self.R = convolutionFilter(beta*kernel)
-    def objective(x):
-        return 0.5*x*self.R.dot(x)
-    def gradient(x):
+    def objective(self, x):
+        obj = 0.5*x*self.R.dot(x)
+        return np.sum(obj)
+    def gradient(self, x):
         return self.R.dot(x)
-    def hessian(x):
+    def hessian(self, x):
         return self.R
+
+class huberPenalty(mirarObjectiveFunction):
+    def __init__(self, delta, beta, neighborhood):
+        self.beta = beta
+        self.delta = delta
+        self.neighborhood = neighborhood
+    def objective(self,x):
+        obj = 0.0*x
+        for iNeighbor in np.arange(self.neighborhood.shape[0]):
+            r = x - np.roll(x, tuple(self.neighborhood[iNeighbor]), tuple(np.arange(len(self.neighborhood[iNeighbor]))))
+            idxLinear = np.abs(r) > self.delta
+            obj[~idxLinear] += 0.5*r[~idxLinear]*r[~idxLinear]
+            obj[idxLinear]  += self.delta*np.abs(r[idxLinear]) - 0.5*self.delta*self.delta
+        return np.sum(self.beta*obj)
+    def gradient(self,x):
+        grad = 0.0*x
+        for iNeighbor in np.arange(self.neighborhood.shape[0]):
+            r = x - np.roll(x, tuple(self.neighborhood[iNeighbor]), tuple(np.arange(len(self.neighborhood[iNeighbor]))))
+            idxLinear = np.abs(r) > self.delta
+            grad[~idxLinear] += r[~idxLinear]
+            grad[idxLinear]  += self.delta*np.sign(r[idxLinear]) 
+        return self.beta*grad
+    class huberHessian(mirarLinearOperator):
+        def __init__(self, huberObjective, xOperatingPoint):
+            self.huberObjective = huberObjective
+            self.xOperatingPoint = xOperatingPoint
+        def dot(self,x): 
+            xOut = 0.0*x
+            for iNeighbor in np.arange(self.huberObjective.neighborhood.shape[0]):
+                rOperatingPoint = self.xOperatingPoint - np.roll(self.xOperatingPoint, tuple(self.huberObjective.neighborhood[iNeighbor]), tuple(np.arange(len(self.huberObjective.neighborhood[iNeighbor]))))
+                idxLinear = np.abs(rOperatingPoint) > self.huberObjective.delta
+                r = x - np.roll(x, tuple(self.huberObjective.neighborhood[iNeighbor]), tuple(np.arange(len(self.huberObjective.neighborhood[iNeighbor]))))
+                xOut[~idxLinear] += r[~idxLinear]
+                xOut[idxLinear]  += self.huberObjective.delta*np.sign(r[idxLinear]) 
+            return self.huberObjective.beta*xOut
+        def Tdot(self,y):
+            return self.dot(x)
+    def hessian(self,x):
+        return self.huberHessian(self, x)
+
+
+
 
 
--- test/test.py
+++ test/test.py
@@ -1,22 +1,28 @@
 import numpy as np
 import mirar
 
-import skimage
+import scipy
+
+import skimage.transform
+import skimage.data
+from matplotlib import pyplot as plt
+
 
 x = np.ones([3, 1],dtype=float)
 matrix = np.ones([5,3],dtype=float)
 A = mirar.denseMatrix(matrix)
-ATAx = A.Tdot(Ax)
+ATAx = A.Tdot(A.dot(x))
 
 x = np.ones([3, 1],dtype=float)
 scale = np.arange(3).astype(float)
 scale.shape = x.shape
-BTBx = B.Tdot(Bx)
+B = mirar.diagonalScale(scale)
+BTBx = B.Tdot(B.dot(x))
 
 x = np.ones([3, 1],dtype=float)
 operators = (A, B)
 AB = mirar.linearProduct(operators)
-BTATABx = AB.Tdot(ABx)
+BTATABx = AB.Tdot(AB.dot(x))
 
 
 x = np.zeros([11,1],dtype=float)
@@ -37,4 +43,72 @@ D.Tdot(D.dot(x))
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+P = skimage.data.shepp_logan_phantom()
+P = skimage.transform.resize(P,[512,512])
+varP = .03
+P = P + np.random.normal(0,np.sqrt(varP),P.shape)
+
+meanX = P
+varX = varP
+invCovX = mirar.diagonalScale(1.0/varX)
+obj_likelihood = mirar.multivariateGaussian(meanX, invCovX)
+
+beta = 10000
+kernel = np.array( [[ 0, -1,  0],
+                    [-1,  4, -1],
+                    [ 0, -1,  0]]).astype(float)
+obj_smoothness1 = mirar.quadraticSmoothness(beta, kernel)
+
+neighborhood = np.array([ (+1, 0),
+                          (-1, 0),
+                          (0, +1),
+                          (0, -1)]).astype(int)
+beta = 1000
+delta = 0.01
+obj_smoothness2 = mirar.huberPenalty(delta, beta, neighborhood)
+
+
+obj_smoothness = obj_smoothness2
+
+obj_joint = mirar.jointObjective( (obj_likelihood, obj_smoothness) )
+
+obj_final = mirar.lexicoVersion(obj_joint, P.shape)
+
+x0 = 0*P
+x0 = P
+
+
+def cBack(xk):
+    print("HELLO: ", obj_final.objective(xk))
+
+
+res = scipy.optimize.minimize(      obj_final.objective, 
+                                    x0, 
+                                    method='Newton-CG', 
+                                    jac=obj_final.gradient, 
+                                    hessp=obj_final.hessp,
+                                    options={'maxiter': 5, 'xtol': 1e-6, 'disp': True},
+                                    callback = cBack)
+
+print("D ", obj_smoothness2.objective(res.x.reshape(P.shape) ) )
+
+plt.figure()
+plt.subplot(1,2,1)
+plt.imshow(res.x.reshape(P.shape) )
+plt.colorbar()
+plt.subplot(1,2,2)
+plt.imshow(P.reshape(P.shape) )
+plt.colorbar()
+plt.show()
 
